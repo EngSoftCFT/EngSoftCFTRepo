08850dc8c6a022db496087e4e81a260ef2b1326a
Pinacolada <mdcmatheusmdc@gmail.com>
2021-03-11 14:28:37 -0300

    Starting Front-End editing

--- ClinicControlCenter/Startup.cs
+++ ClinicControlCenter/Startup.cs
@@ -14,6 +14,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using SDK.EntityRepository;
 
 namespace ClinicControlCenter
 {
@@ -92,6 +93,8 @@ namespace ClinicControlCenter
             services.AddRazorPages();
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration => { configuration.RootPath = "ClientApp/dist"; });
+
+            services.AddScoped(typeof(EntityRepository<>));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
